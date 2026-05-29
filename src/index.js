778743e05d26b853b99e8bc0e46e32f094035b23
francoeffec <84579824+francoeffec@users.noreply.github.com>
2026-05-29 18:13:49 -0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const { loadConfig } = require("./config");
+const { SheetsClient } = require("./sheets/client");
+const { Repository } = require("./sheets/repository");
+const { OpsService } = require("./ops/service");
+const { createSlackApp } = require("./slack/app");
+const { attachRoutes } = require("./server");
+const { startScheduler } = require("./jobs/scheduler");
+
+async function main() {
+  const config = loadConfig();
+  const sheetsClient = await SheetsClient.create({
+    spreadsheetId: config.google.spreadsheetId,
+    serviceAccountJson: config.google.serviceAccountJson
+  });
+  const repository = new Repository(sheetsClient);
+  const opsService = new OpsService({ repository, slackClient: null, config });
+  const slack = createSlackApp({ config, opsService });
+  opsService.slackClient = slack.app.client;
+
+  attachRoutes({ receiver: slack.receiver, config, opsService, repository, sheetsClient });
+  startScheduler({ config, repository });
+  await slack.app.start(config.port);
+  console.log(`Near AI Services OS running on port ${config.port}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
